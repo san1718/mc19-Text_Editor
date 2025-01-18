5ffb9240c16b87c6a57e7954a2f47898b213a133
Sung Min An <an.sungmin17@icloud.com>
2025-01-18 14:16:23 -0800

    install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,8 +11,20 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // Implements a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    
+    const eventPrompt = window.deferredPrompt;
+    if (!eventPrompt) {
+        return;
+    }
+    // Shows prompt and resets deferred prompts, once
+    eventPrompt.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.toggle("hidden", true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clears the prompts
+    window.deferredPrompt = null;
+
+    console.log('Installation complete', event);
+});
